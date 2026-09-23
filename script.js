8834f1461a35cb6311065eeb30204da47e9e1efd
balaraviips-sketch <bala.ravi.ips@gmail.com>
2026-09-23 23:03:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,224 @@
+
+// //Variable var
+
+// var YourName="Ravikumar"
+// console.log(YourName)
+
+// var YourAge=32
+// console.log(YourAge)
+
+// var value
+// value=100
+// console.log(value)
+
+// var a=20
+// a=40
+// console.log(a)
+
+// var a= 50
+// a=80
+// console.log(a)
+
+
+// var Name="Ravikumar"
+// var Age=32
+// var Area="Madurai"
+// console.log(Name)
+// console.log(Age)
+// console.log(Area)
+
+// var College="Mku-Madurai"
+// console.log(College)
+
+// var FavSubject="Maths"
+// console.log(FavSubject)
+
+// var b=35
+// b=75
+// b=105
+// b=150
+// console.log(b)
+
+
+// var Salary=15000
+// Salary=25000
+// console.log(Salary)
+
+
+//Variable let
+
+// let username="Sivam Chits"
+// console.log(username)
+
+// let age=32
+// console.log(age)
+
+// let c
+// c=50
+// console.log(c)
+
+// let d=500
+// d=700
+// console.log(d)
+
+
+// let Name="Ravikumar"
+// let Qualification="BscCs"
+// let locat="Madurai"
+// console.log(Name)
+// console.log(Qualification)
+// console.log(locat)
+
+// let course="JavaScript"
+// console.log(course)
+
+
+// let marks=50
+// marks=80
+// console.log(marks)
+
+// let CompanyName="Stackly"
+// console.log(CompanyName)
+
+// let expieriance="7 years"
+// console.log(expieriance)
+
+// let MobileModel="Motorola 70 Fusion"
+// console.log(MobileModel)
+
+// variable conts
+
+// const Uname="B.RAVIKUMAR"
+// console.log(Uname)
+
+// const DoB="08/10/1994"
+// console.log(DoB)
+
+// const CompName="The Stackly"
+// console.log(CompName)
+
+// const Country="India"
+// console.log(Country)
+
+// const own="Ravikumar"
+// const age=32
+// const city="Madurai"
+// console.log(own)
+// console.log(age)
+// console.log(city)
+
+// const CourseName="JavaScript"
+// console.log(CourseName)
+
+// const CollegeName="MKU-Madurai"
+// console.log(CollegeName)
+
+// const FavColor="RED"
+// console.log(FavColor)
+
+
+// const EmpID="STK-26-6209"
+// // console.log(EmpID)
+
+// const OfficeLocation="Coimbatore"
+// console.log(OfficeLocation)
+
+//Printing Statement
+
+// var a=100
+// console.log(a)
+
+// let NAme="Ravikumar B"
+// console.log(NAme)
+
+// const age=32
+// const Qualifi="Bsc-ComputerScience"
+// console.log(age)
+// console.log(Qualifi)
+
+// let topic="Welcome To Java Script"
+// console.log(topic)
+
+// var a= 5
+// let b= 10
+// const c=15
+// var d=20
+// let e=25
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+// console.log(e)
+
+// const userName="Ravikumar"
+// console.log(userName)
+
+// var username="Anjanbrk"
+// console.log(username)
+
+// let userAge=32
+// console.log(userAge)
+
+// var name="Ravi"
+// let age=32
+// const city="Madurai"
+// var Qualification="Bsc-CS"
+
+// console.log(name)
+// console.log(age)
+// console.log(city)
+// console.log(Qualification)
+
+
+//Popup Method
+// let w="Welcome to JavaSCript"
+// alert(w)
+
+// var name="Ravikumar"
+// alert(name)
+
+// var user=prompt("What is Your Name?")
+// alert(user)
+
+// let age=prompt("What Is Your Age?")
+// console.log(age)
+
+// let Qualifi=prompt("What is Yout Qualification?")
+// alert(Qualifi)
+
+// let area=prompt("Where are you From?")
+// console.log(area)
+
+// let JS=prompt("Do You Know JavaScript?")
+// confirm(JS)
+
+// let contue=prompt("Do You Want to Continue?")
+// confirm(contue)
+
+//document.writeln() +console Methods
+
+// var hi="Hello Everyone"
+// document.writeln(hi)
+
+// var nme="Ravikumar"
+// let Quali="Bsc-CS"
+// const city="Madurai"
+
+// console.log(nme)
+// console.warn(Quali)
+// console.error(city)
+
+
+//Create a small user Information TAsk:
+
+let Uname=prompt("What is your User Name?")
+let aGe=prompt("What is your Age?")
+let qualifi=prompt("What is your Qualification?")
+
+console.log(Uname)
+console.warn(aGe)
+console.error(qualifi)
+// document.writeln(Uname)
+// document.writeln(aGe)
+// document.writeln(qualifi)
+document.writeln(Uname,aGe,qualifi)
